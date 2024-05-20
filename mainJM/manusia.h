0219c7dc6446e46815020cebcd46c023e8b5153c
HaritsTama <nayotama1818@gmail.com>
2024-05-20 10:53:37 +0700

    membuat header file manusia

---
+++ mainJM/manusia.h
@@ -0,0 +1,22 @@
+#ifndef MANUSIA_H
+#define MANUSIA_H
+
+using namespace std;
+
+class manusia
+{
+public:
+	string name;
+	jantung varJantung;
+
+	manusia(string name)
+		: nama(pName)
+	{
+		cout << name << " mati\n";
+	}
+	~manusia()
+	{
+		cout << name << " mati\n";
+	}
+};
+#endif
