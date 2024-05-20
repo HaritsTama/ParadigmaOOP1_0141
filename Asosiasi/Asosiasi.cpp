a94ecee6ef8a1f7fcc583d01a8252aed225e7510
HaritsTama <nayotama1818@gmail.com>
2024-05-20 19:01:34 +0700

    membuat fungsi main dan mengakses proedur tambahDokter, cetakDokter, tambahPasien, cetakPasien melalui class dokter dan pasien

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -5,6 +5,7 @@ using namespace std;
 class dokter;
 class pasien //asosiasi
 {
+public:
 	string nama;
 	vector<dokter*> daftar_dokter;
 	pasien(string pNama) :nama(pNama)
@@ -23,7 +24,7 @@ class dokter
 {
 public:
 	string nama;
-	vector<pasien> daftar_pasien;
+	vector<pasien*> daftar_pasien;
 	
 	dokter(string pNama) :nama(pNama)
 	{
@@ -37,3 +38,56 @@ public:
 	void tambahPasien(pasien*);
 	void cetakPasien();
 };
+
+void pasien::tambahDokter(dokter* pDokter)
+{
+	daftar_dokter.push_back(pDokter);
+}
+void pasien::cetakDokter()
+{
+	cout << "Daftar Dokter yang manangani pasien \"" << this->nama << "\":n";
+	for (auto& a : daftar_dokter) // auto digunakan dalam perulangan for untuk ssecara otomatis menentukan tipe data dari elemen yang diterima
+	{
+		cout << a->nama << "\n";
+	}
+	cout << endl;
+}
+void dokter::tambahPasien(pasien* pPasien)
+{
+	daftar_pasien.push_back(pPasien);
+	pPasien->tambahDokter(this);
+}
+void dokter::cetakPasien()
+{
+	cout << "Daftar pasien dari dokter \"" << this->nama << "\":\n";
+	for (auto& a : daftar_pasien)
+	{
+		cout << a -> nama << "\n";
+	}
+	cout << endl;
+}
+
+int main()
+{
+	dokter* varDokter1 = new dokter("dr.Budi");
+	dokter* varDokter2 = new dokter("dr.Tono");
+	pasien* varPasien1 = new pasien("Andi");
+	pasien* varPasien2 = new pasien("Lia");
+
+	varDokter1->tambahPasien(varPasien1);
+	varDokter1->tambahPasien(varPasien2);
+	varDokter2->tambahPasien(varPasien1);
+
+	varDokter1->cetakPasien();
+	varDokter2->cetakPasien();
+	varPasien1->cetakDokter();
+	varPasien2->cetakDokter();
+
+	delete varPasien1;
+	delete varPasien2;
+	delete varDokter1;
+	delete varDokter2;
+
+	return 0;
+
+}
